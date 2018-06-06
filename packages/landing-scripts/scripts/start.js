0d22b16c29ca22bc87b8a2604cf0c2d94aa9e83d
Kevin Dantas <kevinds29@gmail.com>
2018-06-06 19:54:06 -0300

    Use https on the webpack-serve

--- packages/landing-scripts/scripts/start.js
+++ packages/landing-scripts/scripts/start.js
@@ -1,6 +1,7 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
+const fs = require('fs-extra');
 const WebpackServe = require('webpack-serve');
 const config = require('../config/webpack.config.dev');
 
@@ -11,11 +12,17 @@ function createDevServer() {
     port: process.env.PORT || 3000,
     quiet: true,
     open: true,
+    https: {
+      key: fs.readFileSync('../security/localhost.key'),
+      cert: fs.readFileSync('../security/localhost.cert'),
+      passphrase: '1231',
+    },
     dev: {
       logLevel: 'silent',
     },
     hot: {
       logLevel: 'silent',
+      https: true,
     },
   });
 }
