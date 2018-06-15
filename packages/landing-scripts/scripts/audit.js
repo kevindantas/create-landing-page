e00c89efdb4738a99a0b24d3064a1c07fa0639f5
Kevin Dantas <kevinds29@gmail.com>
2018-06-15 12:54:31 -0300

    Report filename with datetime instead of Date.now

--- packages/landing-scripts/scripts/audit.js
+++ packages/landing-scripts/scripts/audit.js
@@ -6,12 +6,11 @@ const lighthousePrinter = require('lighthouse/lighthouse-cli/printer');
 const chromeLauncher = require('chrome-launcher');
 
 
-function launchChromeAndRunLighthouse(url, opts, config = null) {
+function launchChromeAndRunLighthouse(url, opts = {}, config = null) {
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then((chrome) => {
     opts.port = chrome.port;
-    console.log(chalk.cyan('Preparing lighthouse report...'));
+    console.log(chalk.cyanBright('Preparing lighthouse report...'));
     return lighthouse(url, opts, config).then((results) => {
-      console.log(opts, 'lighthouse runned');
       // use results.lhr for the JS-consumeable output
       // https://github.com/GoogleChrome/lighthouse/blob/master/typings/lhr.d.ts
       // use results.report for the HTML/JSON/CSV output as a string
@@ -21,21 +20,38 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
   });
 }
 
+function getDatetime() {
+  const now = new Date();
+  // Date
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  // Time
+  const hour = now.getHours().toString().padStart(2, '0');
+  const minute = now.getMinutes().toString().padStart(2, '0');
+  const second = now.getSeconds().toString().padStart(2, '0');
+
+  return `${year}${month}${day}${hour}${minute}${second}`;
+}
+
 
 module.exports = function audit() {
   const serveScriptPath = path.join(__dirname, './serve');
   const serve = require(serveScriptPath);
-  console.log(chalk.cyan('Starting a local server to audit on lighthouse...'));
+  console.log(chalk.cyanBright('Starting a local server to audit on lighthouse...'));
   serve(({ urls, server }) => {
-    // Usage:
-    const opts = {
-      chromeFlags: ['--show-paint-rects']
-    };
+    console.log(chalk.cyanBright('Preparing chrome...'));
+    launchChromeAndRunLighthouse(urls.localUrlForBrowser).then((results) => {
+      const reportDate = getDatetime();
+      const auditsPath = path.join(process.cwd(), './audits');
+      const reportFilename = `lighthouse-report-${reportDate}.html`;
+      const reportPath = path.join(auditsPath, reportFilename);
 
-    console.log(chalk.cyan('Lauching chrome...'));
-    launchChromeAndRunLighthouse(urls.localUrlForBrowser, opts).then((results) => {
-      fs.ensureDirSync(path.join(process.cwd(), './audits'));
-      lighthousePrinter.write(results, 'html', `./audits/lighthouse-report-${Date.now()}.html`);
+      fs.ensureDirSync(auditsPath);
+
+      lighthousePrinter.write(results, 'html', reportPath);
+      console.log(chalk.greenBright('Lghthouse reports generated: '));
+      console.log(chalk.default.greenBright(`Report path: \t /audits/${reportFilename}`));
       server.close();
     });
   });
