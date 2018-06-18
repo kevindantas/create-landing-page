b4c8575f08062d72ea8ed7536a1d451b9e1daceb
kevindantas <kevinds29@gmail.com>
2018-06-18 02:55:05 -0300

    Add manifest on build

--- packages/landing-scripts/scripts/build.js
+++ packages/landing-scripts/scripts/build.js
@@ -2,12 +2,21 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
 const webpack = require('webpack');
+const path = require('path');
 const fs = require('fs-extra');
 const paths = require('../config/paths');
 const config = require('../config/webpack.config.prod');
 
+function copyPublicFolder() {
+  fs.copySync(
+    path.join(paths.appPublic),
+    path.join(paths.appOutput),
+  );
+}
+
 module.exports = function build() {
   fs.emptyDirSync(paths.appOutput);
+  copyPublicFolder();
   const compiler = webpack(config);
   compiler.run();
 };
